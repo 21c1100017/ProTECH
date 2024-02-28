f54a01e616afcec26e7c7f29aefab92c6bc65ab8
吉田 健人 <21c1100017yk@edu.tech.ac.jp>
2024-02-29 05:40:37 +0900

    フルパスに変更

--- web_start.sh
+++ web_start.sh
@@ -7,8 +7,8 @@
 --listen-port 443 \
 --set termlog_verbosity=info \
 --set block_global=false \
--s protech.py \
---certs *=../cert.pem \
+-s /etc/protech/master/protech.py \
+--certs *=/etc/protech/cert.pem \
 --ssl-insecure \
 --web-host 0.0.0.0 \
 --web-port 8080
